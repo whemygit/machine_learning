9fbcc99057437f0b2f9f5334368497abdbad280c
whemygit <whemy@sina.com>
2017-03-31 19:00:38 +0800

    0331

---
+++ DecisionTree/tree.py
@@ -0,0 +1,109 @@
+#!/usr/bin/env python
+# -- coding: utf-8 --
+import sys
+
+reload(sys)
+sys.setdefaultencoding("utf-8")
+if __name__ == '__main__':
+    pass
+
+
+#计算给定数据集的香侬熵
+from math import log
+import operator
+
+def calcShannonEnt(dataSet):
+    numEntries=len(dataSet)
+    labelCounts={}
+    for featVec in dataSet:
+        currentLabel=featVec[-1]
+        if currentLabel not in labelCounts.keys():
+            labelCounts[currentLabel]=1                                   #没有的时候先创建，初始值为0？？？？？应该为1
+        labelCounts[currentLabel]+=1
+    shannonEnt=0.0
+    for key in labelCounts:
+        prob=float(labelCounts[key])/numEntries
+        shannonEnt-=prob*log(prob,2)
+    return shannonEnt
+
+########################################################################################################################
+#测试
+def createDataSet():
+    dataSet=[[1,1,'yes'],
+             [1,1,'yes'],
+             [1,0,'no'],
+             [0,1,'no'],
+             [0,1,'no']]
+    labels=['no surfacing','flippers']
+    return dataSet,labels
+
+myDat,labels=createDataSet()
+print myDat
+# print calcShannonEnt(myDat)
+
+########################################################################################################################
+#按照给定特征划分数据集
+def splitDataSet(dataSet,axis,value):
+    '''dataSet,待划分的数据集；axis，划分数据集的特征，即列；value，特征值，按列划分的依据，[axis]==value时的item表现'''
+    retDataSet=[]
+    for featVec in dataSet:
+        if featVec[axis]==value:
+            reducedFeatVec=featVec[:axis]
+            reducedFeatVec.extend(featVec[axis+1:])               #去掉axis列，并且,axis列的值等于value的矩阵
+            retDataSet.append(reducedFeatVec)
+    return retDataSet
+
+# print splitDataSet(myDat,1,1)
+# print splitDataSet(myDat,0,1)
+
+
+#选择最好的数据集划分方式
+def chooseBestFeatureToSplit(dataSet):
+    '''根据信息增益，即分类前与分类后熵值的差值（差值最大）衡量选择分类特征，初始值为分类标签'''
+    numFeatures=len(dataSet[0])-1
+    baseEntropy=calcShannonEnt(dataSet)
+    bestInfoGain=0.0;bestFeature=-1
+    for i in range(numFeatures):
+        featList=[example[i] for example in dataSet]
+        uniqueVals=set(featList)
+        newEntropy=0.0
+        for value in uniqueVals:
+            subDataSet=splitDataSet(dataSet,i,value)
+            prob=len(subDataSet)/float(len(dataSet))
+            newEntropy+=prob*calcShannonEnt(subDataSet)                  #分类后的矩阵期望熵
+        infoGain=baseEntropy-newEntropy
+        if (infoGain>bestInfoGain):
+            bestInfoGain=infoGain
+            bestFeature=i
+    return bestFeature
+
+print chooseBestFeatureToSplit(myDat)
+
+def majorityCnt(classList):
+    classCount={}
+    for vote in classList:
+        if vote not in classCount.keys():classCount[vote]=1
+        classCount[vote]+=1
+    sortedClassCount=sorted(classCount.iteritems(),key=operator.itemgetter(1),reverse=True)            #字典转换为列表，倒序排序
+    return sortedClassCount[0][0]
+
+#创建树
+def createTree(dataSet,labels):
+    classList=[example[-1] for example in dataSet]
+    if classList.count(classList[0])==len(classList):
+        return classList[0]
+    if len(dataSet[0])==1:
+        return majorityCnt(classList)
+    bestFeat=chooseBestFeatureToSplit(dataSet)
+    bestFeatLabel=labels[bestFeat]
+    myTree={bestFeatLabel:{}}
+    del (labels[bestFeat])
+    featValues=[example[bestFeat] for example in dataSet]
+    uniqueVals=set(featValues)
+    for value in uniqueVals:
+        subLabels=labels[:]
+        myTree[bestFeatLabel][value]=createTree(splitDataSet
+                                                (dataSet,bestFeat,value),subLabels)
+    return myTree
+
+print createTree(myDat,labels)
--- KNN/knn.py
+++ KNN/knn.py
@@ -6,7 +6,7 @@ reload(sys)
 sys.setdefaultencoding("utf-8")
 if __name__ == '__main__':
     pass
-######################################################################################
+########################################################################################################################
 #实验数据
 from numpy import *
 import operator
@@ -41,38 +41,100 @@ def classify0(inX,dataSet,labels,k):
 b=classify0([0,0],group,labels,3)
 # print b
 
-#####################################################################################
+########################################################################################################################
 # examples:improving matches from a dating site with kNN
 
 #text record to Numpy Parsing code
+from numpy import *
 def file2matrix(filename):
-    import numpy
     '''读取文件数据，前三列读为矩阵，最后一列读为标签向量'''
     with open(filename) as fr:
-        numberOfLines=len(fr.readlines())
+        lines=fr.readlines()                                                        #重新赋值给变量很重要，否则还需再打开一次（即还需要执行line56）
+        numberOfLines=len(lines)
         returnMat=zeros((numberOfLines,3))
         classLabelVector=[]
-        with open(filename) as fr:                                                #一定要重新打开一次！！！！！！！不然写不进去。。。。。。。。
-            index=0
-            for line in fr.readlines():
-                line=line.strip()
-                listFromLine=line.split('\t')
-                returnMat[index,:]=listFromLine[0:3]
-                classLabelVector.append(int(listFromLine[-1]))
-    print shape(returnMat)
-    print len(classLabelVector)
+        # with open(filename) as fr:                                                #一定要重新打开一次！！！！！！！不然写不进去。。。。。。。。
+        index=0
+        for line in lines:
+            line=line.strip()
+            listFromLine=line.split('\t')
+            returnMat[index,:]=listFromLine[0:3]
+            classLabelVector.append(int(listFromLine[-1]))
+            index+=1
+    # print shape(returnMat)
+    # print len(classLabelVector)
     return returnMat,classLabelVector
-
 
 #获取数据
 datingDataMat,datingLabels=file2matrix("D://myfile/machine_learning_code/machinelearninginaction/Ch02/datingTestSet2.txt")
 # print datingDataMat
-
+# print datingLabels
+# print datingDataMat[2][1]
 
 #图形展示
-import matplotlib
-import matplotlib.pyplot as plt
-fig=plt.figure()                             #创建图表对象
-ax=fig.subplot(111)                          #添加坐标
-ax.scatter(datingDataMat[:,1],datingDataMat[:,2],15.0*array(datingLabels),15.0*array(datingLabels))
-plt.show()
+# import matplotlib
+# import matplotlib.pyplot as plt
+# fig=plt.figure()                             #创建图表对象
+# ax=fig.add_subplot(111)                          #当前图表对象位置,等同于（1,1,1）
+# ax.scatter(datingDataMat[:,1],datingDataMat[:,2],15.0*array(datingLabels),15.0*array(datingLabels))
+# plt.show()
+
+
+#为了减小各变量量值不同的影响，进行归一化处理
+def autoNorm(dataSet):
+    # print dataSet
+    minVals=dataSet.min(0)                 #0表示列方向,计算每列最小值
+    # print minVals
+    maxVals=dataSet.max(0)
+    ranges=maxVals-minVals                  #计算每列极差
+    normDataset=zeros(shape(dataSet))
+    m=dataSet.shape[0]
+    normDataset=dataSet-tile(minVals,(m,1))  #构建（m,1）结构的矩阵
+    normDataset=normDataset/tile(ranges,(m,1))
+    return normDataset,ranges,minVals
+
+
+normMat,ranges,minVals=autoNorm(datingDataMat)
+# print normMat
+# print ranges
+# print minVals
+
+########################################################################################################################
+#分类器针对约会网站的测试代码
+def datingClassTest():
+    hoRatio=0.10
+    datingDataMat,datingLabels=file2matrix('D://myfile/machine_learning_code/machinelearninginaction/Ch02/datingTestSet2.txt')
+    normMat,ranges,minVals=autoNorm(datingDataMat)
+    m=normMat.shape[0]
+    numTestVecs=int(m*hoRatio)
+    errorCount=0.0
+    for i in range(numTestVecs):
+        classfierResult=classify0(normMat[i,:],normMat[numTestVecs:m,:],
+                                  datingLabels[numTestVecs:m],3)
+        print "the classifier came back with: %d,the real answer is: %d" % (classfierResult,datingLabels[i])
+        if classfierResult!=datingLabels[i]:errorCount+=1.0
+    print "the total error rate is: %f" %(errorCount/float(numTestVecs))
+
+# datingClassTest()
+
+############################################################################################################
+#约会网站预测函数
+def classifyPerson():
+    resultList=['not at all','in small doses','in large doses']
+    percentTats=float(raw_input(
+        "percentage of time spent playing video games"
+    ))
+    ffMiles=float(raw_input("frequent fliter miles earned per year"))
+    iceCream=float(raw_input("liters of ice cream consumed per year"))
+    datingDataMat,datingLabels=file2matrix('D://myfile/machine_learning_code/machinelearninginaction/Ch02/datingTestSet2.txt')
+    normMat,ranges,minVals=autoNorm(datingDataMat)
+    inArr=array([ffMiles,percentTats,iceCream])
+    classfierResult=classify0((inArr-minVals)/ranges,normMat,datingLabels,3)
+    print "you will probably like this person:",resultList[classfierResult-1]
+
+# classifyPerson()
+
+
+
+
+
